1212b7b3ed725ead7c8dfe8e598457d425fe9fed
kaclarpt <kaclarpt@126.com>
2019-04-22 00:58:56 +0800

    简单招聘页面样式

--- pages/recruit/employees/employees.js
+++ pages/recruit/employees/employees.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    contactorImg: "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2873914150,902943197&fm=26&gp=0.jpg",
+    employees: [
+      {
+        name: "张金书",
+        skill: "Java | ANDROID",
+        description: "IT互联网 | 研发",
+        avatar: "http://i9.taou.com/maimai/p/1215/7755_11_CbwworvRM1rdZZ-a160",
+      }, {
+        name: "林昱政",
+        skill: "Java",
+        description: "IT互联网 | 研发",
+        avatar: "http://i9.taou.com/maimai/p/2395/4843_86_w2NkfGEQR0BMhe-a160"
+      }, {
+        name: "顾旭华",
+        skill: "Java",
+        description: "IT互联网 | 研发",
+        avatar: "http://i9.taou.com/maimai/p/1012/427_117_41QYxTvRYBt2Fy-a160"
+      },
+    ],
+    job: {
+      name: "JAVA开发工程师",
+      companyName: "美团",
+      companyStatus: "已上市",
+      workerNum: "1万人以上",
+      position: "上海",
+      salary: "30k-50k",
+      experience: "5-10年",
+      education: "本科及以上",
+      img: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555840965613&di=875b3342115d716112326515ec2ce07e&imgtype=0&src=http%3A%2F%2Fimages.liqucn.com%2Fimg%2Fh22%2Fh82%2Fimg_localize_960ebfd8ef6df0321effec15e78d2db4_220x220.png",
+      person: "周会",
+      identity: "美团招聘经理"
+    },
     name: "JAVA开发工程师",
     companyName: "美团",
     companyStatus: "已上市",
